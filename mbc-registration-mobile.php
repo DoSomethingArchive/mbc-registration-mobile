395010f39ad0f4bd5f9044f7c61b090e4a0b57ca
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-08-01 17:08:06 -0400

    Move to using MB_Configuration as sigleton pattern

--- mbc-registration-mobile.php
+++ mbc-registration-mobile.php
@@ -12,24 +12,13 @@ define('CONFIG_PATH',  __DIR__ . '/messagebroker-config');
 
 // Load up the Composer autoload magic
 require_once __DIR__ . '/vendor/autoload.php';
-use DoSomething\MBC_RegistrationMobile\MBC_RegistrationMobile;
-
-use DoSomething\StatHat\Client as StatHat;
-use DoSomething\MB_Toolbox\MB_Toolbox;
+use DoSomething\MBC_RegistrationMobile\MBC_RegistrationMobile_Consumer;
 
 // Load configuration settings specific to this application
 require_once __DIR__ . '/mbc-registration-mobile.config.inc';
 
-// Create objects for injection into MBC_ImageProcessor
-$mb = new MessageBroker($credentials, $config);
-$sh = new StatHat([
-  'ez_key' => $settings['stathat_ez_key'],
-  'debug' => $settings['stathat_disable_tracking']
-]);
-$tb = new MB_Toolbox($settings);
-
-
 // Kick off - block, wait for messages in queue
 echo '------- mbc-registration-mobile START - ' . date('j D M Y G:i:s T') . ' -------', PHP_EOL;
-$mb->consumeMessage(array(new MBC_RegistrationMobileConsumer($mb, $sh, $tb, $settings), 'consumeRegistrationMobileQueue'));
+$mb = $mbConfig->getProperty('messageBroker');
+$mb->consumeMessage(array(new MBC_RegistrationMobile_Consumer(), 'consumeRegistrationMobileQueue'));
 echo '------- mbc-registration-mobile END - ' . date('j D M Y G:i:s T') . ' -------', PHP_EOL;
