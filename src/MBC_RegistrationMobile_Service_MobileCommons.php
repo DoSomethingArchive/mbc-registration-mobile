48404b752856efb743864172bd6ead6d71829656
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-08-05 04:18:06 -0400

    Functional Mobile Commons API submissions

--- src/MBC_RegistrationMobile_Service_MobileCommons.php
+++ src/MBC_RegistrationMobile_Service_MobileCommons.php
@@ -1,9 +1,13 @@
 <?php
-
+/**
+ * Service class specific to the Mobile Commons SMS service.
+ * https://www.mobilecommons.com
+ */
 namespace DoSomething\MBC_RegistrationMobile;
 
 use DoSomething\StatHat\Client as StatHat;
 use DoSomething\MB_Toolbox\MB_Toolbox;
+use DoSomething\MB_Toolbox\MB_Configuration;
 
 /*
  * MBC_RegistrationMobile_Service_MobileCommons: Used to process the mobileCommonsQueue
@@ -11,6 +15,20 @@ use DoSomething\MB_Toolbox\MB_Toolbox;
  */
 class  MBC_RegistrationMobile_Service_MobileCommons extends MBC_RegistrationMobile_BaseService
 {
+
+  /**
+   * Singleton instance of MB_Configuration application settings and service objects
+   *
+   * @var object
+   */
+  protected $mbConfig;
+
+  /**
+   * Message Broker connection to RabbitMQ
+   *
+   * @var object
+   */
+  protected $messageBroker;
   
   /**
    * StatHat object for logging of activity
@@ -28,43 +46,34 @@ class  MBC_RegistrationMobile_Service_MobileCommons extends MBC_RegistrationMobi
   protected $toolbox;
 
   /**
-   * Setting from external services - Mail chimp.
+   * Connection to mobile service to send message details to.
+   */
+  protected $mobileServiceObject;
+
+  /**
+   * Value of message from queue to be consumed / processed.
    *
    * @var array
    */
-  protected $settings;
+  protected $message;
   
-  /**
-   * Values submitted in potential mobile activity.
-   */
-  protected $mobileSubmission;
-  
-  /**
-   * Values submitted in potential mobile activity.
-   */
-  protected $opt_in_path_id;
-
   /**
    * Constructor for MBC_BaseConsumer - all consumer applications should extend this base class.
-   *  
-   * @param object $statHat
-   *   Track application activity by triggering counters in StatHat service.
    *
-   * @param object $toolbox
-   *   A collection of common tools for the Message Broker system.
-   *   
-   * @param array $settings
-   *   Settings from internal and external services used by the application.
+   * @param array $message
+   *  The payload of the unseralized message being processed.
    */
-  public function __construct(StatHat $statHat, MB_Toolbox $toolbox, $settings, $opt_in_path_id) {
+  public function __construct($message) {
 
-    $this->messageBroker = $messageBroker;
-    $this->statHat = $statHat;
-    $this->toolbox = $toolbox;
-    $this->settings = $settings;
-    $this->opt_in_path_id = $opt_in_path_id;
+    $this->mbConfig = MB_Configuration::getInstance();
+    $this->messageBroker = $this->mbConfig->getProperty('messageBroker');
+    $this->statHat = $this->mbConfig->getProperty('statHat');
+    $this->toolbox = $this->mbConfig->getProperty('mbToolbox');
+
+    $this->mobileServiceObject = $this->connectServiceObject($message['application_id']);
+    $this->message = $message;
   }
-  
+
   /**
    * Method to determine if message can be processed. Tests based on requirements of the service.
    *
@@ -73,15 +82,15 @@ class  MBC_RegistrationMobile_Service_MobileCommons extends MBC_RegistrationMobi
    *
    * @retun boolean
    */
-  private function canProcess($message) {
+  public function canProcess($message) {
 
     // Mobile Commons, current supplier for US and CA requirements
-    if (!isset($this->mobileSubmission['mobile'])) {
-      echo '** Mobile Commons requires a mobile number for processing.', PHP_EOL;
+    if (!isset($message['mobile'])) {
+      echo '** canProcess(): Mobile Commons requires a mobile number for processing.', PHP_EOL;
       return FALSE;
     }
-    if (!isset($this->mobileSubmission['service_path_id'])) {
-      echo '** Mobile Commons requires service_path_id (opt in) for processing.', PHP_EOL;
+    if (!isset($message['service_path_id'])) {
+      echo '** canProcess(): Mobile Commons requires service_path_id (opt in) for processing.', PHP_EOL;
       return FALSE;
     }
 
@@ -94,44 +103,84 @@ class  MBC_RegistrationMobile_Service_MobileCommons extends MBC_RegistrationMobi
    * @param array $message
    *  The payload of the unseralized message being processed.
    */
-  private function setter($message) {
+  public function setter($message) {
 
-    $this->mobileSubmission = $message;
+    $this->message['phone_number'] = $message['mobile'];
+    unset($this->message['mobile']);
+    if (isset($message['service_path_id'])) {
+      $this->message['mc_opt_in_path_id'] = $message['service_path_id'];
+      unset($this->message['service_path_id']);
+    }
+
+    if (strtoupper($message['application_id']) == 'CGG2014' && isset($message['original']['candidate_name'])) {
+      $this->message['CGG2014_1st_vote'] = $message['original']['candidate_name'];
+    }
+    // AGG2015
+    if (strtoupper($message['application_id']) == 'AGG' && isset($message['original']['candidate_name'])) {
+      $this->message['AGG2015_1st_vote'] = $message['original']['candidate_name'];
+      $this->message['AGG2015_1st_vote_id'] = $message['original']['candidate_id'];
+      $this->message['AGG2015_1st_vote_gender'] = $message['original']['candidate_gender'];
+    }
+
   }
 
   /**
    * Process message from consumed queue.
    */
-  private function process() {
-    
+  public function process() {
+
+    $payload = $this->message['payload'];
+    unset($this->message['payload']);
+    unset($this->message['original']);
+
     try {
-      $config = array(
-        'username' => getenv("MOBILE_COMMONS_USER"),
-        'password' => getenv("MOBILE_COMMONS_PASSWORD"),
-      );
-      $MobileCommons = new MobileCommons($config);
-      $status = $MobileCommons->profiles_update($args);
 
-      if (isset($status->error)) {
-        echo 'Error - ' . print_r($status->error, TRUE), "\n";
-        echo 'Submitted: ' . print_r($args, TRUE), "\n\n";
+      $status = (array)$this->mobileServiceObject->profiles_update($this->message);
+      if (isset($status['error'])) {
+        echo '- Error - ' . $status['error']->attributes()->{'message'} , PHP_EOL;
+        echo '  Submitted: ' . print_r($this->message, TRUE), PHP_EOL;
+        $this->statHat->ezCount('MBC_RegistrationMobile_Service_MobileCommons: profiles_update error: ' . $status['error']->attributes()->{'message'});
       }
+      else {
 
-      // @todo: Watch opted_out_source in response from Mobile Commons to log
-      // possible reason for profile addition/update failing.
-
-      echo '-> MBC_RegistrationMobile->profiles_update mobile: ' . $payloadDetails['mobile'] . ' -------', PHP_EOL;
-
-      $this->messageBroker->sendAck($payload);
-      $this->statHat->addStatName('profiles_update success');
+        // todo: adjust exchange / queue settings to require ack
+        // $this->messageBroker->sendAck($payload);
+        $this->statHat->ezCount('MBC_RegistrationMobile_Service_MobileCommons: profiles_update success');
+      }
+      echo '-> MBC_RegistrationMobile_Service_MobileCommons->process: ' . $this->message['mobile'] . ' -------', PHP_EOL;
     }
     catch (Exception $e) {
       trigger_error('mbc-registration-mobile ERROR - Failed to submit "profiles_update" to Mobile Commons API.', E_USER_WARNING);
       echo 'Excecption:' . print_r($e, TRUE), PHP_EOL;
-      $this->statHat->addStatName('profiles_update error');
-      $this->messageBroker->sendAck($payload);
+      $this->statHat->ezCount('MBC_RegistrationMobile_Service_MobileCommons: profiles_update error');
     }
 
+  }
+
+  /**
+   * connectService: instantiate object for mobile service based on the
+   * country code.
+   *
+   * @param string $affiliate
+   *   The country code of the site that generated the message and thus the mobile service
+   *   that needs to be connected to.
+   *
+   * @return object $mobileServiceObject
+   *   An obect of a mobile service.
+   */
+  public function connectServiceObject($affiliate) {
+
+    $mobileCommonsConfig = $this->mbConfig->getProperty('mobileCommons_config');
+
+    // @todo: trap undefined $affiliate values.
+    $config = array(
+      'username' => $mobileCommonsConfig[$affiliate]['username'],
+      'password' => $mobileCommonsConfig[$affiliate]['password'],
+      'company_key' => $mobileCommonsConfig[$affiliate]['company_key'],
+    );
+    $mobileServiceObject = new \MobileCommons($config);
+
+    return $mobileServiceObject;
   }
 
 }
