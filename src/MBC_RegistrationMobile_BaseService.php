ba470bc93d07544d7f9c22408c758d48b32854f6
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-08-05 03:46:03 -0400

    Add suport for MB_Configuration class as singleton

--- src/MBC_RegistrationMobile_BaseService.php
+++ src/MBC_RegistrationMobile_BaseService.php
@@ -1,9 +1,14 @@
 <?php
+/**
+ * Base abstract class to provide a template for active class to extend
+ * from. Structure based on factory pattern.
+ */
 
-namespace DoSomething\MB_Toolbox;
+namespace DoSomething\MBC_RegistrationMobile;
 
 use DoSomething\StatHat\Client as StatHat;
 use DoSomething\MB_Toolbox\MB_Toolbox;
+use DoSomething\MB_Toolbox\MB_Configuration;
 
 /*
  * MBC_UserAPICampaignActivity.class.in: Used to process the transactionalQueue
@@ -12,8 +17,12 @@ use DoSomething\MB_Toolbox\MB_Toolbox;
 abstract class MBC_RegistrationMobile_BaseService
 {
 
-   // The number of seconds to pause when throttling is triggered
-   const THROTTLE_TIMEOUT = 5;
+  /**
+   * Singleton instance of MB_Configuration application settings and service objects
+   *
+   * @var object
+   */
+  protected $mbConfig;
 
   /**
    * Message Broker connection to RabbitMQ
@@ -21,14 +30,14 @@ abstract class MBC_RegistrationMobile_BaseService
    * @var object
    */
   protected $messageBroker;
-  
+
   /**
    * StatHat object for logging of activity
    *
    * @var object
    */
   protected $statHat;
-  
+
   /**
    * Message Broker Toolbox - collection of utility methods used by many of the
    * Message Broker producer and consumer applications.
@@ -37,13 +46,6 @@ abstract class MBC_RegistrationMobile_BaseService
    */
   protected $toolbox;
 
-  /**
-   * Setting from external services - Mail chimp.
-   *
-   * @var array
-   */
-  protected $settings;
-  
   /**
    * Value of message from queue to be consumed / processed.
    *
@@ -54,26 +56,19 @@ abstract class MBC_RegistrationMobile_BaseService
   /**
    * Constructor for MBC_BaseConsumer - all consumer applications should extend this base class.
    *
-   * @param object $messageBroker
-   *   The Message Broker object used to interface the RabbitMQ server exchanges and related queues.
-   *  
-   * @param object $statHat
-   *   Track application activity by triggering counters in StatHat service.
-   *
-   * @param object $toolbox
-   *   A collection of common tools for the Message Broker system.
-   *   
-   * @param array $settings
-   *   Settings from internal and external services used by the application.
+   * @param array $message
+   *   The message to process by the service from the connected queue.
    */
-  public function __construct($messageBroker, StatHat $statHat, MB_Toolbox $toolbox, $settings) {
+  public function __construct($message) {
 
-    $this->messageBroker = $messageBroker;
-    $this->statHat = $statHat;
-    $this->toolbox = $toolbox;
-    $this->settings = $settings;
+    $this->mbConfig = MB_Configuration::getInstance();
+    $this->messageBroker = $this->mbConfig->getProperty('messageBroker');
+    $this->statHat = $this->mbConfig->getProperty('statHat');
+    $this->toolbox = $this->mbConfig->getProperty('mbToolbox');
+
+    $this->message = $message;
   }
-  
+
   /**
    * Method to determine if message can be processed. Tests based on requirements of the service.
    *
@@ -82,7 +77,7 @@ abstract class MBC_RegistrationMobile_BaseService
    *
    * @retun boolean
    */
-  abstract protected function canProcess($message);
+  abstract public function canProcess($message);
 
   /**
    * Sets values for processing based on contents of message from consumed queue.
@@ -90,11 +85,11 @@ abstract class MBC_RegistrationMobile_BaseService
    * @param array $message
    *  The payload of the unseralized message being processed.
    */
-  abstract protected function setter($message);
+  abstract public function setter($message);
 
   /**
    * Process message from consumed queue.
    */
-  abstract protected function process();
+  abstract public function process();
 
 }
