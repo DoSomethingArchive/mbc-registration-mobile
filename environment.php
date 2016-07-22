9a8a1ceb3cc4d060e3028e3ffad2431ca2f022aa
Darren Dee Lee <darrendouglas.lee@gmail.com>
2016-07-22 01:55:41 -0600

    Add gulp and php_codesniffer

---
+++ environment.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Define the current enviroment that the application is in. See allowedEnviroment() in
+ * mbp-user-import_ ... .php for valid values. Typically "local", "dev" and "prod".
+ */
+define('ENVIRONMENT', 'local');
