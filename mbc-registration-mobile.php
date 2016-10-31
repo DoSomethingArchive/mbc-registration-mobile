daf67bb6fff66f5b38d59d8964327902e0e0498f
Sergii Tkachenko <hi@sergii.org>
2016-10-31 19:49:09 +0200

    Allow thor environment

--- mbc-registration-mobile.php
+++ mbc-registration-mobile.php
@@ -52,7 +52,8 @@ function allowedEnviroment($setting)
     $allowedEnviroments = [
         'local',
         'dev',
-        'prod'
+        'thor',
+        'prod',
     ];
 
     if (in_array($setting, $allowedEnviroments)) {
